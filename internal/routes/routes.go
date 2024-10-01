c9738d6bc1f65332e045b6e48f4b8a12cdeefba9
Umut Sahin Onder <umutsonder@gmail.com>
2024-10-02 00:23:12 +0200

    added addHandler for POST /add

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,19 +1,62 @@
 package routes
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func NewRouter() http.Handler {
 	router := http.NewServeMux()
-	router.HandleFunc("/echo/{id}", echoHandler)
+	router.HandleFunc("POST /echo/{id}", echoHandler)
+	router.HandleFunc("POST /add", addHandler)
 
 	return router
 }
 
+type Operation struct {
+	Number1 int `json:"number1"`
+	Number2 int `json:"number2"`
+}
+
+type OperationResult struct {
+	Result int `json:"result"`
+}
+
+type SumOperation struct {
+	Numbers []int `json:"numbers"`
+}
+
 func echoHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Received request %s %s", r.Method, r.URL.Path)
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	id := r.PathValue("id")
 	w.Write([]byte("Recieved request for item: " + id))
+	logger.Info(fmt.Sprintf("Received request for item: %s", id))
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	var numbers Operation
+
+	err := json.NewDecoder(r.Body).Decode(&numbers)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		logger.Error("Failed to Decode response", slog.String("error", err.Error()))
+		return
+	}
+
+	result := OperationResult{Result: numbers.Number1 + numbers.Number2}
+	response, err := json.Marshal(result)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		logger.Error("Failed to marshal response", slog.String("error", err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+
+	w.Header().Set("Content-Type", "application/json")
+	logger.Info("Successfully added two numbers", slog.Int("result", result.Result))
 }
--- internal/routes/routes_test.go
+++ internal/routes/routes_test.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -34,5 +36,59 @@ func TestEchoHandler(t *testing.T) {
 			t.Fatalf("Expected response to be Recieved request for item: %d, got %s", tt.expected, string(body))
 		}
 
+	}
+}
+
+func TestAddHandler(t *testing.T) {
+	type TestOperation struct {
+		Number1 any `json:"number1"`
+		Number2 any `json:"number2"`
+	}
+	tests := []struct {
+		data     TestOperation
+		expected int
+	}{
+		{TestOperation{1, 2}, 3},
+		{TestOperation{2, 3}, 5},
+		{TestOperation{0, 0}, 0},
+		{TestOperation{-1, 1}, 0},
+		{TestOperation{-1, -1}, -2},
+		{TestOperation{string('a'), string('b')}, 195}, // TODO: expected must be something related to "Bad Request"
+	}
+	for _, tt := range tests {
+		client := &http.Client{}
+
+		// Encode the data to JSON
+		jsonData, err := json.Marshal(tt.data)
+		if err != nil {
+			t.Fatalf("Failed to marshal JSON: %v", err)
+		}
+
+		// Create a new HTTP request
+		req, err := http.NewRequest("POST", "http://localhost:8080/add", bytes.NewBuffer(jsonData))
+		if err != nil {
+			t.Fatalf("Failed to create request: %v", err)
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		// Send the request usung the HTTP client
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("Failed to send request: %v", err)
+		}
+		defer req.Body.Close()
+
+		// Read and Decode the response
+		// TODO: Handle non-json response for invalid types of value (Bad Request)
+
+		var result OperationResult
+
+		err = json.NewDecoder(resp.Body).Decode(&result)
+		if err != nil {
+			t.Fatalf("Failed to decode response: %v", err)
+		}
+		if result.Result != tt.expected {
+			t.Fatalf("Expected response to be %d, got %d", tt.expected, result.Result)
+		}
 	}
 }
