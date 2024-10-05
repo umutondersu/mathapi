2faf0cc6f9df8d55f113aa80b97fe01f631edfae
Umut Sahin Onder <umutsonder@gmail.com>
2024-10-05 14:23:47 +0200

    implemented Substracthandler and made the routes more modular by diving core features into their own functions

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -11,8 +11,9 @@ import (
 
 func NewRouter() http.Handler {
 	router := http.NewServeMux()
-	router.HandleFunc("POST /echo/{id}", echoHandler)
-	router.HandleFunc("POST /add", addHandler)
+	router.HandleFunc("GET /echo/{id}", handleEcho)
+	router.HandleFunc("POST /add", handleAdd)
+	router.HandleFunc("POST /substract", handleSubstract)
 
 	return router
 }
@@ -30,42 +31,83 @@ type SumOperation struct {
 	Numbers []int `json:"numbers"`
 }
 
-func echoHandler(w http.ResponseWriter, r *http.Request) {
+func handleEcho(w http.ResponseWriter, r *http.Request) {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	id := r.PathValue("id")
 	w.Write([]byte("Recieved request for item: " + id))
 	logger.Info(fmt.Sprintf("Received request for item: %s", id))
 }
 
-func addHandler(w http.ResponseWriter, r *http.Request) {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	var numbers Operation
-
+func checkInput(op *Operation, w http.ResponseWriter, r *http.Request, logger *slog.Logger) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
-	err := decoder.Decode(&numbers)
+	err := decoder.Decode(&op)
 	if err != nil {
 		var unmarshalTypeError *json.UnmarshalTypeError
 		if errors.As(err, &unmarshalTypeError) {
 			http.Error(w, "number1 and number2 must be numbers", http.StatusBadRequest)
 			logger.Error("number1 and number2 must be numbers", slog.String("error", err.Error()))
+			return errors.New("number1 and number2 must be numbers")
 		} else {
 			http.Error(w, "Bad Request", http.StatusBadRequest)
 			logger.Error("Failed to decode request body", slog.String("error", err.Error()))
+			return errors.New("Failed to decode request body")
 		}
-		return
 	}
+	return nil
+}
 
-	result := OperationResult{Result: numbers.Number1 + numbers.Number2}
+func marshalResult(result OperationResult, w http.ResponseWriter, logger *slog.Logger) ([]byte, error) {
 	response, err := json.Marshal(result)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		logger.Error("Failed to marshal response", slog.String("error", err.Error()))
-		return
+		return nil, errors.New("Failed to marshal response")
 	}
+	return response, nil
+}
+
+func prepareResponse(w http.ResponseWriter, response []byte) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
 
 	w.Header().Set("Content-Type", "application/json")
+}
+
+func handleAdd(w http.ResponseWriter, r *http.Request) {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	var numbers Operation
+
+	if err := checkInput(&numbers, w, r, logger); err != nil {
+		return
+	}
+
+	result := OperationResult{Result: numbers.Number1 + numbers.Number2}
+	response, err := marshalResult(result, w, logger)
+	if err != nil {
+		return
+	}
+
+	prepareResponse(w, response)
+
 	logger.Info("Successfully added two numbers", slog.Int("result", result.Result))
+}
+
+func handleSubstract(w http.ResponseWriter, r *http.Request) {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	var numbers Operation
+
+	if err := checkInput(&numbers, w, r, logger); err != nil {
+		return
+	}
+
+	result := OperationResult{Result: numbers.Number1 - numbers.Number2}
+	response, err := marshalResult(result, w, logger)
+	if err != nil {
+		return
+	}
+
+	prepareResponse(w, response)
+
+	logger.Info("Successfully substracted two numbers", slog.Int("result", result.Result))
 }
--- internal/routes/routes_test.go
+++ internal/routes/routes_test.go
@@ -10,6 +10,16 @@ import (
 	"testing"
 )
 
+type TestOperation struct {
+	Number1 any `json:"number1"`
+	Number2 any `json:"number2"`
+}
+
+type TestOperationinvalidkeys struct {
+	Numb1 any `json:"numb1"`
+	Numb2 any `json:"numb2"`
+}
+
 func TestEchoHandler(t *testing.T) {
 	tests := []struct {
 		expected int
@@ -41,16 +51,6 @@ func TestEchoHandler(t *testing.T) {
 }
 
 func TestAddHandler(t *testing.T) {
-	type TestOperation struct {
-		Number1 any `json:"number1"`
-		Number2 any `json:"number2"`
-	}
-
-	type TestOperationinvalidkeys struct {
-		Numb1 any `json:"numb1"`
-		Numb2 any `json:"numb2"`
-	}
-
 	tests := []struct {
 		data     any
 		expected any
@@ -79,6 +79,71 @@ func TestAddHandler(t *testing.T) {
 
 		// Create a new HTTP request
 		req, err := http.NewRequest("POST", "http://localhost:8080/add", bytes.NewBuffer(jsonData))
+		if err != nil {
+			t.Fatalf("Failed to create request: %v", err)
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		// Send the request usung the HTTP client
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("Failed to send request: %v", err)
+		}
+		defer req.Body.Close()
+
+		// Check for non-JSON response for invalid types of value (Bad Request)
+		if resp.StatusCode == http.StatusBadRequest {
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("Failed to read response body: %v", err)
+			}
+			if strings.TrimSpace(string(body)) != tt.expected {
+				t.Fatalf("For Bad Request Expected response to be %s, got %s", tt.expected, string(body))
+			}
+			return
+		}
+
+		// Read and Decode the response JSON
+		var result OperationResult
+		err = json.NewDecoder(resp.Body).Decode(&result)
+		if err != nil {
+			t.Fatalf("Failed to decode response to TestOperationResult: %v", err)
+		}
+		if result.Result != tt.expected {
+			t.Fatalf("Expected response to be %d, got %d", tt.expected, result.Result)
+		}
+	}
+}
+
+func TestSubstractdHandler(t *testing.T) {
+	tests := []struct {
+		data     any
+		expected any
+	}{
+		{TestOperation{3, 1}, 2},
+		{TestOperation{0, 0}, 0},
+		{TestOperation{3, 5}, -2},
+		{TestOperation{-1, 1}, -2},
+		{TestOperation{-1, -1}, 0},
+		{TestOperation{5, string('b')}, "number1 and number2 must be numbers"},
+		{TestOperation{string('a'), string('b')}, "number1 and number2 must be numbers"},
+		{TestOperation{string('a'), 5}, "number1 and number2 must be numbers"},
+		{TestOperationinvalidkeys{1, 2}, "Bad Request"},
+		{TestOperationinvalidkeys{string('a'), 2}, "Bad Request"},
+		{TestOperationinvalidkeys{string('b'), string('a')}, "Bad Request"},
+		{"This is a string request", "Bad Request"},
+	}
+	for _, tt := range tests {
+		client := &http.Client{}
+
+		// Encode the data to JSON
+		jsonData, err := json.Marshal(tt.data)
+		if err != nil {
+			t.Fatalf("Failed to marshal JSON: %v", err)
+		}
+
+		// Create a new HTTP request
+		req, err := http.NewRequest("POST", "http://localhost:8080/substract", bytes.NewBuffer(jsonData))
 		if err != nil {
 			t.Fatalf("Failed to create request: %v", err)
 		}
